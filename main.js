40a196a48644186f3f3618f09b739d45d435e901
jwkim4870 <jwkim4870@gmail.com>
2019-09-25 17:37:36 +0900

    dd

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ $(document).ready( function() {
     c.fillColor = 'break';
     var text = new PointText( 200, 200 );
     text.justification = 'center'; text.fillColor = 'white'; text.fontSize = 20;
-    text.center = 'Hello, World!!!';
+    text.content = 'Hello, World!!!';
 
     tool.onMouseDown = function( event ) {
         var c = Shape.Circle( event.point, 20 );
