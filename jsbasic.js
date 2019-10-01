a892757d93eabde802b5755ec09f6c110d7d53b1
jwkim4870 <jwkim4870@gmail.com>
2019-10-01 10:25:18 +0900

    d

--- jsbasic.js
+++ jsbasic.js
@@ -1,10 +1,10 @@
 // jsbasic.js
-document.write( "<h3>JavaScript 기본 언어 스펙 연습<h3>");
+document.write( "<h3>JavaScript 기본 언어 스펙 연습</h3>");
 docuement.write( "<hr>" );
 
 var box;
 box = 100;
-document.write( box, "<br" );
+document.write( box, "<br>" );
 console.log( box );
 document.write( "<hr>" );
 
