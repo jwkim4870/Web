f2fc5a9efae90e4197455acc6aac6c40f0e202b4
jwkim4870 <jwkim4870@gmail.com>
2019-10-01 13:22:03 +0900

    d

---
+++ js-0bject.js
@@ -0,0 +1,23 @@
+// 내장 객체 생성
+var tv = new Object();
+tv.color = "white";
+tv.price = 30000;
+tv.info = function() {
+    document.write( "tv color : " + this.color, "<br>" );
+    document.write( "tv price : " + this.price, "<br>" );
+}
+
+var car = {
+    color: "black",
+    price: 50000,
+    info: function() {
+        document.write( "car color : " + this.color, "<br>" );
+        document.write( "car price : " + this.price, "<br>" );
+    }
+};
+document.write( "<h3>tv object call method</h3>" );
+tv.info();
+document.write( "<hr>" );
+document.write( "<h3>car object call method</h3>" );
+car.info();
+document.write( "<hr>" );
