84f95a7a2eefb6d4b6ac65a914bfa2954cc9fffd
jwkim4870 <jwkim4870@gmail.com>
2019-10-01 11:31:49 +0900

    ㅇ

--- jsbasic.js
+++ jsbasic.js
@@ -77,3 +77,19 @@ document.write( "<hr>" );
 document.write( k === 10, "<br>" );
 document.write( m === 10, "<br>" );  // ===: 값과 타입도 같은지 비교
 document.write( "<hr>" );
+
+// 입력
+var number1 = prompt( "정수1 입력", "0" );
+var number2 = prompt( "정수2 입력", "0" );
+var sum = parseInt( number1 ) + parseInt( number2 );
+
+document.write( number1 + " + " + number2 + " = " + sum, "<br>" );
+document.write( "<hr>" );
+
+var select = confirm( "회원을 탈퇴하시겠습니까?" );
+
+if ( select ) {
+    document.write( "탈퇴 처리되었습니다." );
+} else {
+    document.write( "탈퇴 취소되었습니다." );
+}
