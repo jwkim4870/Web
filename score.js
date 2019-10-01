c3c7d9e33abdd665b7a1ecd669310d609a326409
jwkim4870 <jwkim4870@gmail.com>
2019-10-01 21:52:50 +0900

    d

--- score.js
+++ score.js
@@ -1,3 +1,27 @@
-function sub_sum(sub1, sub2, sub3) {
-    return sum = sub1 + sub2 + sub3;
+function student  () {
+    var name = document.getElementById(name).value;
+    var sub1 = document.getElementById(sub1).value;
+    var sub2 = document.getElementById(sub2).value;
+    var sub3 = document.getElementById(sub3).value;
+
+    sum(sub1, sub2, sub3);
+
+    var str = "<tr>";
+    str += "<td>" + name + "</td>";
+    str += "<td>" + sub1 + "</td>";
+    str += "<td>" + sub2 + "</td>";
+    str += "<td>" + sub3 + "</td>";
+    str += "<td>" + sum + "</td>";
+    str += "<td>" +  + "</td>";
+    str += "<td>" +  + "</td>";
+    str += "<td>" +  + "</td>";
+    str += "</tr>";
+
+    var score_tbody = document.getElementById(score_tbody);
+    score_tbody.innerHTML = score_tbody.innerHTML + str;
+}
+
+function sum (sub1, sub2, sub3) {
+    var total = parseInt(sub1) + parseInt(sub2) + parseInt(sub3);
+    return total;
 }
